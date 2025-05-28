38bded645b57be86113f2bf37c41a32212179a22
Arnau Gil Gil <162440885+XxArnauGxX@users.noreply.github.com>
2025-05-29 00:38:17 +0200

    solucionar archivo de entrada y quitar deprecated funciones del mongodb connect

--- src/config/db.js
+++ src/config/db.js
@@ -6,10 +6,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(MONGODB_URI);
     console.log('✅ MongoDB connected successfully');
   } catch (e) {
     console.error('❌ Error connecting to MongoDB:', e.message);
