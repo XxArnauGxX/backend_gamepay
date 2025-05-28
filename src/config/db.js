2d92405c3337078c256ec6ab73a35f00584eb2f5
Arnau Gil Gil <162440885+XxArnauGxX@users.noreply.github.com>
2025-05-29 00:24:01 +0200

    añadir archivos de creación de servidor y conexión a la db

---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('✅ MongoDB connected successfully');
+  } catch (e) {
+    console.error('❌ Error connecting to MongoDB:', e.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
