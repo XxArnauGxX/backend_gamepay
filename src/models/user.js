7f924f16281150b7061ce33045029be2fcaafa3f
Arnau Gil Gil <162440885+XxArnauGxX@users.noreply.github.com>
2025-05-29 01:15:24 +0200

    añadir api de movies y implementar modelos de user y product

---
+++ src/models/user.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const cartItemSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      default: 1,
+      min: 1,
+    },
+    selected: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { _id: false }
+);
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  address: String,
+  refreshToken: String,
+  cart: [cartItemSchema],
+});
+
+export default mongoose.model('User', userSchema);
