c1589eba6437df93ec3461ce866d5732da90ea3b
Arnau Gil Gil <162440885+XxArnauGxX@users.noreply.github.com>
2025-05-29 11:35:44 +0200

    añadir rutas de usuario y base para centralizar

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import router from './routes/baseRoutes.js';
 
 dotenv.config();
 
@@ -8,5 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/api', router);
 
 export default app;
